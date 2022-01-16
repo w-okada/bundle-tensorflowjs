7778f79006f67d9a866c80b890aeb86f222d7b63
Wataru Okada <wataru.okada@flect.co.jp>
2022-01-17 08:21:52 +0900

    update

--- bisenetv2-js/webpack.config.js
+++ bisenetv2-js/webpack.config.js
@@ -1,30 +1,37 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
 
 const manager = {
-    mode: 'production',
-    entry: './src/index.ts',
-    resolve: {
-        extensions: [".ts", ".js"],
-        fallback: {
-            "os": false
-        }
+  mode: "production",
+  entry: "./src/index.ts",
+  resolve: {
+    extensions: [".ts", ".js"],
+    fallback: {
+      os: false,
+      buffer: require.resolve("buffer"),
     },
-    module: {
-        rules: [
-            { test: /\.ts$/, loader: 'ts-loader' },
-            { test:/resources\/.*\.bin/, type:"asset/inline"},
-            { test:/resources\/.*\.json/, type:"asset/source"}            
-        ],
-    },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
-        libraryTarget: 'umd',
-        globalObject: 'typeof self !== \'undefined\' ? self : this'
-    },
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: "ts-loader" },
+      { test: /resources\/.*\.bin/, type: "asset/inline" },
+      { test: /resources\/.*\.json/, type: "asset/source" },
+    ],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "umd",
+    globalObject: "typeof self !== 'undefined' ? self : this",
+  },
+  plugins: [
+    // Work around for Buffer is undefined:
+    // https://github.com/webpack/changelog-v5/issues/10
+    // https://stackoverflow.com/questions/68707553/uncaught-referenceerror-buffer-is-not-defined
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+  ],
 };
 
-module.exports = [
-    manager
-];
-
+module.exports = [manager];
